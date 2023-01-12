b718c84ffd5f24fe5d081ee485b6b977c8d827b7
Sachin <sogoduraju@google.com>
2023-01-12 10:19:59 +0000

    removed building permits

--- building-permits-Inspection-backend/src/routers/index.ts
+++
@@ -1,7 +0,0 @@
-import dialogflow from "./dialogflowRouter"
-
-
-const routes = [dialogflow]
-
-
-export default routes
--- building-permits-Inspection-backend/src/utils/firebase-admin-con.ts
+++
@@ -1,8 +0,0 @@
-import admin from "firebase-admin"
-import { getFirestore } from "firebase-admin/firestore"
-
-if (admin.apps.length === 0) {
-  admin.initializeApp()
-}
-const db = getFirestore()
-export { db }
